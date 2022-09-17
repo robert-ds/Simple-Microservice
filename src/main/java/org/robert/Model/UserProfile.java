9ffc8756d9d07e9e864c497754355ab4869f36d0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-17 15:21:58 -0400

    create class POJO UserProfile

--- src/main/java/org/robert/Model/UserProfile.java
+++ src/main/java/org/robert/Model/UserProfile.java
@@ -6,5 +6,55 @@ package org.robert.Model;
  * Date: 17/9/22
  * Time: 3:07 p. m.
  */
+
 public class UserProfile {
+
+  private long ProId;
+  private String FName;
+  private String LName;
+  private String Add;
+
+  public UserProfile(){}
+
+  public UserProfile(long ProId, String FName, String LName, String Add){
+    this.ProId = ProId;
+    this.FName = FName;
+    this.LName = LName;
+    this.Add = Add;
+  }
+
+  // Getters and Setters
+
+  public long getProId(){
+    return this.ProId;
+  }
+
+  public void setProId(long ProId){
+    this.ProId = ProId;
+  }
+
+  public String getFName(){
+    return this.FName;
+  }
+
+  public void setFName(String FName){
+    this.FName = FName;
+  }
+
+  public String getLName(){
+    return this.LName;
+  }
+
+  public void setLName(String LName){
+    this.LName = LName;
+  }
+
+  public String getAdd(){
+    return this.Add;
+  }
+
+  public void setAdd(String Add){
+    this.Add = Add;
+  }
+
 }
