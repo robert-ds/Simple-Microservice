5ae0f11af57e64a9f35fbce1bb3fe00b85a624b8
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-17 15:25:21 -0400

    This class represent the data base

--- src/main/java/org/robert/DAO/DataBaseClass.java
+++ src/main/java/org/robert/DAO/DataBaseClass.java
@@ -1,10 +1,22 @@
 package org.robert.DAO;
 
+import org.robert.Model.UserProfile;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
  * Date: 17/9/22
  * Time: 3:05 p. m.
  */
+
 public class DataBaseClass {
+  private static Map<Long, UserProfile> messages = new HashMap<Long,UserProfile>();
+
+  public static Map<Long,UserProfile> getUsers(){
+    return messages;
+  }
+
 }
